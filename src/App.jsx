864b50e2479e36851bf718db6de0ece415dc0f1b
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-25 22:22:05 +0530

    uuid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(uuid());
   return (
     <div>
       hello
