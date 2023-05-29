cf2beb44d1ad6f392f3e9e2a27d0df5f6495941f
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-29 22:57:23 +0530

    css updated

--- src/util/utllity.js
+++ src/util/utllity.js
@@ -1,5 +1,12 @@
 export function makeBold() {
-  
+    const selectedText = window.getSelection().toString();
+
+    if (selectedText.length > 0) {
+        const range = window.getSelection().getRangeAt(0);
+        const newNode = document.createElement("span");
+        newNode.classList.add('bold');
+        range.surroundContents(newNode);
+    }
 }
 export function makeItalic() {
 
